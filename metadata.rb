9656a65a27da26e6e2e9357ad33d15c1b8feccdc
Bob Webber <webber@patientslikeme.com>
2015-10-19 11:14:32 -0400

    HA proxy recipe added, old apache2 cookbook removed

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'plm-haproxy'
+maintainer       'PatientsLikeMe, Inc.'
+maintainer_email 'cookbooks@patientslikeme.com'
+license          'Apache 2.0'
+description      'Installs/Configures plm-haproxy'
+long_description 'Installs/Configures plm-haproxy'
+version          '0.1.0'
+
+depends "haproxy-ng"
---
+++ recipes/default.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: plm-haproxy
+# Recipe:: default
+#
+# Copyright (C) 2015 PatientsLikeMe, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package 'haproxy'
+package 'haproxyctl'
+package 'hatop'
+
+directory '/etc/ssl/private/haproxy' do
+  action :create
+  owner 'root'
+  group 'sysadmin'
+  mode '0440'
+end
+
+cookbook_file '/etc/haproxy/haproxy.cfg' do
+  action :create
+  owner 'root'
+  group 'sysadmin'
+  mode '0640'
+  source 'haproxy.cfg'
+  notifies :restart, 'service[haproxy]', :delayed
+end
+
+# template '/etc/ssl/private/haproxy.pem' do
+#   action :create
+#   owner 'root'
+#   group 'root'
+#   mode '0400'
+#   source 'haproxy_keys.pem'
+#   notifies :restart, 'service[haproxy]', :delayed
+# end
+
+service 'haproxy' do
+  action :enable
+  supports :restart => true, :reload => true, :status => true
+end
+  
+
