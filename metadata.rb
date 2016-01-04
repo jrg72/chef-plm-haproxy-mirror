83e832fe8e59ddb5d90dfc04b6ed06f9cb3e5f68
Bryan Alves <bryanalves@gmail.com>
2016-01-04 17:57:42 -0500

    Move haproxy recipe to plm-website cookbook
    The plm-website application cookbook should be the store for all
    application recipes.  This allows us to more easily share things like
    users (in this case).

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@patientslikeme.com'
 license          'Apache 2.0'
 description      'Installs/Configures plm-haproxy'
 long_description 'Installs/Configures plm-haproxy'
-version          '0.1.0'
+version          '0.1.1'
 
 depends "haproxy-ng"
 depends 'plm', '~> 0.1'
