776488e95f3ae6d1604a3fca8e6ee343ac89985e
Bryan Alves <bryanalves@gmail.com>
2016-06-20 15:37:42 -0400

    Version bump: 0.6.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@patientslikeme.com'
 license          'Apache 2.0'
 description      'Installs/Configures plm-haproxy'
 long_description 'Installs/Configures plm-haproxy'
-version          '0.5.0'
+version          '0.6.0'
 
 issues_url       'https://github.com/patientslikeme/chef-plm-haproxy/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/patientslikeme/chef-plm-haproxy' if respond_to?(:source_url)
